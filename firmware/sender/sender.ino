e2bde764ed68924d262c8a52f0016521f0c33d2e
Max Kendall <66796793+mpkendall@users.noreply.github.com>
2026-05-14 17:27:25 -0400

    initialize

---
+++ firmware/sender/sender.ino
@@ -0,0 +1,68 @@
+#include <WiFi.h>
+#include <esp_now.h>
+
+// ****** REPLACE WITH YOUR RECEIVER MAC ADDRESS ******
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+// ****** REPLACE WITH YOUR RECEIVER MAC ADDRESS ******
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message {
+  char msg[32];
+  int a;
+  float b;
+  bool c;
+} struct_message;
+
+struct_message packetData;
+
+// peer info instantiation
+esp_now_peer_info_t peerInfo;
+ 
+void setup() {
+  Serial.begin(115200);
+ 
+  WiFi.mode(WIFI_STA);
+
+  // initialization
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // configure packet callback
+  esp_now_register_send_cb(OnDataSent);
+  
+  // register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+   
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+ 
+void loop() {
+  strcpy(packetData.msg, "Hello Stasis!");
+  packetData.a = random(1,20);
+  packetData.b = 3.14;
+  packetData.c = false;
+  
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &packetData, sizeof(packetData));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(2000);
+}
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\nMessage Attempted: ");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Fail");
+}
